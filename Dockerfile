e2cddb8436655d51fce7e03162e78b6c845557d5
Евгений <avikon3412@yandex.ru>
2023-05-14 11:21:13 +0300

    добавил тест RepositoryMoneyTest

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM openjdk:16-alpine
 EXPOSE 5500
 ENV TZ Europe/Moscow
 ADD /target/TransferMoneyService-1.0-SNAPSHOT.jar app.jar
-ENTRYPOINT ["java","-jar","app.jar"]
+ENTRYPOINT ["java","-jar","/app.jar"]
 
--- src/test/java/ru/netology/repositoryTest/RepositoryMoneyTest.java
+++ src/test/java/ru/netology/repositoryTest/RepositoryMoneyTest.java
@@ -9,6 +9,7 @@ import ru.netology.model.Amount;
 import ru.netology.model.Card;
 import ru.netology.repository.RepositoryMoney;
 import ru.netology.web.request.RequestTransfer;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -22,30 +23,71 @@ public class RepositoryMoneyTest {
     public static final int ID = 1;
     public static final String CONFIRMATION_CODE = "8998";
     public static final RequestTransfer REQUEST_TRANSFER = new RequestTransfer("1111222233334444",
-            "5555666677778888","01/27","555",new Amount(10000,"RUB"));
-    public static final Card TEST_CARD_FROM = new Card("5555333344440000","01/26","666",new Amount(
-            800000,"RUB"));
-    public static final Card TEST_CARD_TO = new Card("9999888877776666","01/26","444",new Amount(
-            9500000,"RUB"));
-    public static final String CARD_NUMBER_FROM = "5555333344440000";
-    public static final String CARD_NUMBER_TO = "9999888877776666";
-@Test
-@DisplayName("Проверка сохранения запроса по адресу /transfer")
-    void saveTransferTest (){
-       String operationId = OPERATION_ID;
-       RequestTransfer requestExp = REQUEST_TRANSFER;
-       repositoryMoney.saveTransfers(operationId,requestExp);
+            "5555666677778888", "01/27", "555", new Amount(10000, "RUB"));
+    public static final Card TEST_CARD = new Card("5555333344440000", "01/26", "666", new Amount(
+            800000, "RUB"));
+    public static final String CARD_NUMBER = "5555333344440000";
 
-    RequestTransfer requestActual = repositoryMoney.getRequestTransfer(operationId);
-    assertEquals(requestExp,requestActual);
-}
 
-@Test
-@DisplayName("Проверка получения сохраненного Request по Id")
-void getRequestTransfer_Test(){
-    String operationId = OPERATION_ID;
-    RequestTransfer requestExp = REQUEST_TRANSFER;
-    RequestTransfer requestActual = repositoryMoney.getRequestTransfer(operationId);
-    assertEquals(requestExp,requestActual);
-}
+    @Test
+    @DisplayName("Проверка сохранения запроса по адресу /transfer")
+    void saveTransferTest() {
+        String operationId = OPERATION_ID;
+        RequestTransfer requestExp = REQUEST_TRANSFER;
+        repositoryMoney.saveTransfers(operationId, requestExp);
+
+        RequestTransfer requestActual = repositoryMoney.getRequestTransfer(operationId);
+        assertEquals(requestExp, requestActual);
+    }
+
+    @Test
+    @DisplayName("Проверка получения сохраненного Request по Id")
+    void getRequestTransferTest() {
+        String operationId = OPERATION_ID;
+        RequestTransfer requestExp = REQUEST_TRANSFER;
+        repositoryMoney.saveTransfers(operationId, requestExp);
+
+        RequestTransfer requestActual = repositoryMoney.getRequestTransfer(operationId);
+        assertEquals(requestExp, requestActual);
+    }
+
+    @Test
+    @DisplayName("Проверка сохранения кода подтверждения карты")
+    void saveCode_Test() {
+        String operationId = OPERATION_ID;
+        String codeExp = CONFIRMATION_CODE;
+        repositoryMoney.saveCode(operationId, codeExp);
+        String codeActual = repositoryMoney.getCode(operationId);
+        assertEquals(codeExp, codeActual);
+    }
+
+    @Test
+    @DisplayName("Проверка получения кода подтверждения карты")
+    void getCodeTest() {
+        String operationId = OPERATION_ID;
+        String codeExp = CONFIRMATION_CODE;
+        repositoryMoney.saveCode(operationId, codeExp);
+        String codeActual = repositoryMoney.getCode(operationId);
+        assertEquals(codeExp, codeActual);
+    }
+
+    @Test
+    @DisplayName("Проверка сохранения карты")
+    void saveCardTest() {
+        Card cartExp = TEST_CARD;
+        String testCardNumber = CARD_NUMBER;
+        repositoryMoney.saveCard(testCardNumber,cartExp);
+        Card cartActual = repositoryMoney.getCard(testCardNumber);
+        assertEquals(cartExp,cartActual);
+    }
+   @Test
+   @DisplayName("Проверка поиска карты")
+    void searchCardTest(){
+       Card cartExp = TEST_CARD;
+       String testCardNumber = CARD_NUMBER;
+       repositoryMoney.saveCard(testCardNumber,cartExp);
+       Card cartActual = repositoryMoney.searchCard(testCardNumber);
+       assertEquals(cartExp,cartActual);
+    }
+
 }
